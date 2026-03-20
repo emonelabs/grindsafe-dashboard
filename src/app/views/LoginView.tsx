3bbe952c1caf1f3680b2927a5b3763fd25a21fa8
João Silva <joao@emonelabs.com>
2026-03-20 19:14:49 -0300

    Update LoginView.tsx

--- src/app/views/LoginView.tsx
+++ src/app/views/LoginView.tsx
@@ -221,7 +221,7 @@ export function LoginView() {
 
             <div className="relative hidden bg-muted md:block">
               <img
-                src={`${import.meta.env.BASE_URL}imgs/login-preview.jpg`
+                src={`${import.meta.env.BASE_URL}imgs/login-preview.jpg`}
                 alt="GrindSafe Preview"
                 className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
               />
